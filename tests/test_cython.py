85345ce7e107b862d6d7ad7104ceda0b796b06fd
Leonid R <leovp@users.noreply.github.com>
2017-03-01 23:30:12 +0300

    Add tests for variable annotations

--- tests/test_cython.py
+++ tests/test_cython.py
@@ -19,3 +19,15 @@ def func(a: int, b: int):
 cpdef object func(int a, int b):
     pass
 ''', mode='cython', do_check=False)
+
+
+def test_variable_annotations():
+    assert_decompiles('''
+def func():
+    x: int = 0
+    y: float
+''', '''
+cpdef object func():
+    cdef int x = 0
+    cdef float y
+''', mode='cython', do_check=False)
